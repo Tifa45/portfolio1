cf68d2d6f35c3050a2e42a46e312eaef6544e562
Mostafa Yousry <m.yousry8806@gmail.com>
2025-09-08 18:22:57 +0300

    the rout again

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-  { path: "sceene-details", element: <SceeneDetails /> },
+  { path: "/sceene-details", element: <SceeneDetails /> },
 ]);
 
 
