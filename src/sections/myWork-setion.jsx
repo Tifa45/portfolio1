ad7b468caa4551606e9580a9e725666a9c51dce5
Mostafa Yousry <m.yousry8806@gmail.com>
2025-09-08 18:19:23 +0300

    editing the link

--- src/sections/myWork-setion.jsx
+++ src/sections/myWork-setion.jsx
@@ -48,7 +48,7 @@ function MyWorkSection() {
               </p>
             </div>
             <div className="flex justify-center p-6">
-              <Link to="sceene-details" target="blank" rel="noopener noreferrer">
+              <Link to="/sceene-details" target="blank" >
                 <MyButton text="See more" />
               </Link>
             </div>
